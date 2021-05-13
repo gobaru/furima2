546f1d6cdfd7d664308ef4341b4a675274d745fb
gobaru <gobaru.1993@gmail.com>
2021-05-13 12:21:45 +0900

    バリデーションの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
   end
@@ -10,5 +11,5 @@ class ItemsController < ApplicationController
   def create
 
   end
-  
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,26 @@ class Item < ApplicationRecord
   belongs_to :item_condition
   belongs_to :prefecture
   belongs_to :shipping_day
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :introduction
+    validates :category_id
+    validates :delivery_fee_id
+    validates :item_condition_id
+    validates :prefecture_id
+    validates :shipping_day_id
+  end
+
+  validates :category_id, :delivery_fee_id, :item_condition_id, :prefecture_id, :shipping_day_id,
+            numericality: { other_than: 1, message: "can't be blank" } 
+
+
+  validates :price, numericality: { greater_than: 299, less_than: 10_000_000 }
+
+  with_options presence: true, format: { with: /\A-?[0-9]+(\.[0-9]+)?\z/, message: 'Half-width number', allow_blank: true } do
+    validates :price
+  end
+
 end
